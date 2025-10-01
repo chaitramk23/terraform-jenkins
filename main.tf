cbc953a2483e9717a5d005765817fa3b46311ac5
chaitramk <cmk6790@gmail.com>
2025-10-01 20:45:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # EC2 Instance
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0cf8ec67f4b13b491"  # ubuntu AMI
+  ami           = "ami-0cf8ec67f4b13b491"  # Amazon Linux 2 AMI
   instance_type = "t2.medium"
    tags = {
     Name = "My-ec2-Server"   # Your custom server name
