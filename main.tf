2503c36ab590f608c24f704a12b02221ce6568de
chaitramk23 <cmk6790@gmail.com>
2025-10-01 20:40:09 +0530

    Initial commit - Terraform EC2 instance

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Provider
+provider "aws" {
+  region = "ap-south-1"  # Change to your region
+}
+
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0cf8ec67f4b13b491"  # ubuntu AMI
+  instance_type = "t2.medium"
+   tags = {
+    Name = "My-ec2-Server"   # 👈 Your custom server name
+  }
+}
+
+
+# Output public IP
+output "instance_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
