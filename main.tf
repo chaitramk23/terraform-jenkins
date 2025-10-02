3c5d592cc3331fd8c5994424ce9a1338c8537df7
chaitramk <cmk6790@gmail.com>
2025-10-02 16:55:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  ami           = "ami-0c02fb55956c7d316"  # You can replace with a valid AMI for your region
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "example-${var.env}"
+  }
 }
