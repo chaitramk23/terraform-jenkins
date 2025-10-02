7a4e7920b424ea7ecfe2ac9390ad10c5ee6ac64b
chaitramk <cmk6790@gmail.com>
2025-10-02 16:26:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
-# Provider
+# ---------- PROVIDER ----------
 provider "aws" {
-  region = "ap-south-1"  # Change to your region
+  region = var.region
 }
 
-# EC2 Instance
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0cf8ec67f4b13b491"  # Amazon Linux 2 AMI
-  instance_type = "t2.medium"
-   tags = {
-    Name = "My-ec2-Server"   # Your custom server name
+# ---------- RESOURCE ----------
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"   # Use region-specific AMI
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "${var.env}-server"
   }
-}
-
-
-# Output public IP
-output "instance_ip" {
-  value = aws_instance.my_ec2.public_ip
 }
