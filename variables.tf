614b0b7fbcc597b1acbbe36f6f2dad709657c9d0
chaitramk <cmk6790@gmail.com>
2025-10-02 16:45:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "ap-south-1"
 }
 
 variable "env" {
-  description = "Deployment environment"
+  description = "Environment name"
   type        = string
+  default     = "dev"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
 }
